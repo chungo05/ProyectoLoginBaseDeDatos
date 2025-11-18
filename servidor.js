49ca87eac39a13cdbfa52651e79f55e528e74a00
chungo05 <Massimochungcr@gmail.com>
2025-11-17 19:59:48 -0600

    Update, fixed registro.html and started doing the login.html

--- servidor.js
+++ servidor.js
@@ -7,7 +7,8 @@ const path = require('path');
 // El traductor de SQL a JS
 const sqlite3 = require('sqlite3');
 // La licuadora de constraseñas
-const bcrypt = require('bycrypt');
+const bcrypt = require('bcryptjs');
+const { hash } = require('crypto');
 
 // -- CREAR LA APP
 
@@ -23,7 +24,7 @@ const dbPath = path.join(__dirname, 'data', 'LaBase.sqlite')
 
 // -- CONECTAR A LA BASE DE DATOS --
 // Creamos una nueva conexión a nuestro acrhivo de base de datos
-const db = new sqlite33.Database(dbPath, (err) => {
+const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error al conectar a la base de datos', err.message);   
     } else {
@@ -62,23 +63,71 @@ app.post('/registrar', (req, res) => {
     db.get(sqlSelect, [usuario], (err, fila) => {
     // 1 if si sale err, res.send
         if(err){
-            return res.send("Error al guardar el usuario en la base de datos");
+            return res.send("Error al guardar el usuario en la base de datos" + err.message);
         }
-    });
-
-    
-
     // 2  si la fila existe, res.send usuario ya existe, decir que mande otro
-
-
-    // 3 la verificación de contraseña de manera local
-
+        if (fila){
+            return res.send("Error, el nombre de usuario ya existe");
+        }
+        // ya paso la verificación de usuario
+        // 3 la verificación de contraseña de manera local, si !==  mandar mensaje de poner otra
+        if(pass1 !== pass2){
+            return res.send("Tu contraseña no coincide, vuelve a intentarlo");
+        }
+        // Ya el usuario y la contraseña se verificaron
+        /**
+         * Hasheamos la contraseña con bcrypt.hash(datos de entrada, la pass y el salt round(err y hash)) => {
+         * dentro de {} esta el if error
+         */
+        bcrypt.hash(pass1, saltRounds, (err, hash) => {
+            if(err){
+                return res.send ("Hubo un error al encriptar tu contraseña" + err.message)
+            }
+        // YA ESTA HASHEADA LA CONTRASEÑA, AHORA TOCA INSERTARLA
+        // HACEMOS EL sqlInsert = "Comando"
+            const sqlInsert = "INSERT INTO Usuarios (Usuario, ClaveHash) VALUES (?, ?)";
+            const datos = [usuario, hash];
+        // db.run ([el Sqlinsert y el hash] (err)) => {}
+            db.run(sqlInsert, datos, (err) => {
+                // if de error 
+                if(err){
+                  return res.send("Error al hacer tu registro, intente de nuevo" + err.message);
+                }
+             // FUNCIONO, MANDA DENTRO DE ESTE RUN EL MENSAJE DE FELICIDADES CON RES.SEND
+                return res.send("FELICIDADES, TU REGISTRO SE HA COMPLETADO")
+            });
+        });
+    });
 });
 
+// --- NUEVA FUNCION: OBTENER LA LISTA DE USUARIOS ---
+// Esta ruta sera para llenar la lista desplegable del login
+// El navegador lo llamara autojmaticamente al cargar la pagina
+app.get('/api/usuarios', (req, res) => {
+    console.log("Alguien pidió la lista de usuarios (/api/usuarios)");
+
+    // 1.- La consulta: SOLO pedimos el nombre, nada de contraseñas
+    const sql = "SELECT Usuario FROM Usuarios";
+
+    // 2.- EJecutar la consulta con db.all()
+    // db all recupera todas las filas que coincidan
+
+    // db all (el sql, [] (error y filas))
+        // if err di que hubo un error al consultar los usuarios +  err.message
+    
+    db.all(sql, [], (err, filas) => {
+        if (err){
+            return res.send ("Hubo un error al consultar los usuarios a la base de datos " + err.message);
+        }
+        // 3.- Enviar la respuesta
+    // con res.json(lo que da el db.all)
+        res.json(filas);
+    });
+});
 
 
 //  ABRIR EL RESTAURANTE (INICIAR EL SERVIDOR)
 //  Le decimos a la 'app' que empeice a ' escuchar' peticiones en el puerto que decidimos antes
 app.listen(PORT, () => {
     console.log(`servidor escuchando en http://localhost:${PORT}`);
-})
+});
